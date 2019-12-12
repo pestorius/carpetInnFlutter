45a79e9bb7133861c79f3048d7a57d0837b6f565
Farhan <farhaangauhar@yahoo.com>
2019-12-12 21:32:15 +0800

    Added background image, changed font, added small text below

--- lib/main.dart
+++ lib/main.dart
@@ -19,44 +19,81 @@ class MyApp extends StatelessWidget {
 class SplashScreen extends StatelessWidget {
   SplashScreen({Key key}) : super(key: key);
   final letterspacing = 3.0;
+  final logofont = 'Lora';
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: <Widget>[
-            // Stroked text as border.
-            Text(
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Image.asset(
+              "assets/splash_screen_rug.jpg",
+              width: size.width,
+              height: size.height,
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Center(
+            child: Text(
               'Carpet Inn',
               style: TextStyle(
-                fontSize: 40,
-                foreground: Paint()
-                  ..style = PaintingStyle.stroke
-                  ..strokeWidth = 5
-                  ..color = Colors.black,
-                letterSpacing: letterspacing,
-                fontFamily: 'Gelasio-Regular'
-              ),
+                  fontSize: 50,
+                  foreground: Paint()
+                    ..style = PaintingStyle.stroke
+                    ..strokeWidth = 4.3
+                    ..color = Colors.black,
+                  letterSpacing: letterspacing,
+                  fontFamily: logofont),
             ),
             // Solid text as fill.
-            Text(
+          ),
+          Center(
+            child: Text(
               'Carpet Inn',
               style: TextStyle(
-                fontSize: 40,
+                  fontSize: 50,
+                  color: Colors.amber,
+                  letterSpacing: letterspacing,
+                  fontFamily: logofont),
+            ),
+          ),
+          Positioned(
+            bottom: 40.0,
+            left: 0.0,
+            right: 0.0,
+            child: Text(
+              'owned by Petal World Sdn. Bhd.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 17.0,
+                  foreground: Paint()
+                    ..style = PaintingStyle.stroke
+                    ..strokeWidth = 4.3
+                    ..color = Colors.black,
+                  letterSpacing: 2.0,
+                  fontFamily: logofont),
+            ),
+            // Solid text as fill.
+          ),
+          Positioned(
+            bottom: 40.0,
+            left: 0.0,
+            right: 0.0,
+            child: Text(
+              'owned by Petal World Sdn. Bhd.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 17.0,
                 color: Colors.amber,
-                letterSpacing: letterspacing,
-                fontFamily: 'Gelasio-Regular'
+                letterSpacing: 2.0,
+                fontFamily: logofont,
               ),
             ),
-          ],
-        )
-        //child: Text('Carpet Inn', style: TextStyle(fontSize: 40.0,
-                                                   //color: Colors.amberAccent),),
+          ),
+        ],
       ),
     );
-
   }
 }
-
-
