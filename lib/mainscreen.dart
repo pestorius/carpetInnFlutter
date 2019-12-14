1de8d5d0db3213f69539c5eb959edcc71ba141ad
Farhan <farhaangauhar@yahoo.com>
2019-12-14 17:14:45 +0800

    POC: Grabbing carpet data from firebase

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'dart:async';
 
 /*void main() {
   runApp(MainScreen());
@@ -20,6 +22,7 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   MainScreen({Key key, this.title}) : super(key: key);
   final String title;
+  final databaseReference = FirebaseDatabase.instance.reference();
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +31,45 @@ class MainScreen extends StatelessWidget {
         title: Text(this.title),
       ),
       body: Center(
-          child:
-          Text(
-            'Hello World',
-          )
+        child: GestureDetector(
+          onTap: () {
+            getRecord();
+          },
+          child: Text('Hello World'),
+        ),
       ),
     );
   }
+
+  void getRecord() {
+    databaseReference.once().then((DataSnapshot snapshot) {
+      var carpets = [];
+      var data = snapshot.value;
+      data.forEach((k,v) => carpets.add(k));
+      print(carpets[0]);
+    });
+
+    /*void getRecord() {
+      var db = databaseReference.child("carpet-inn");
+      db.once().then((DataSnapshot snapshot){
+        Map<dynamic, dynamic> values = snapshot.value;
+        values.forEach((key,values) {
+          print(values);
+        });
+      });
+    }*/
+  }
+
+  /*static Future<int> getUserAmount() async {
+    final response = await FirebaseDatabase.instance
+        .reference()
+        .child("22")
+        .once();
+    var users = [];
+    response.value.forEach((v) => users.add(v));
+    print(users);
+    return users.length;
+  }*/
 }
+
+
