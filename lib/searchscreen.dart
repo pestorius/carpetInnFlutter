20466f06a0d9f29c175aab5e4b106e2977a4ff9e
Farhan Aslam <pestorius@Farhans-MacBook-Air.local>
2020-05-03 17:12:36 +0800

    Adjusted search bar size and margin

--- lib/searchscreen.dart
+++ lib/searchscreen.dart
@@ -63,9 +63,9 @@ class SearchScreen extends StatelessWidget {
                 delegate: CarpetSearch(combinedList: combinedList));
           },
           child: Container(
-            height: 55.0,
+            height: 50.0,
             width: double.infinity,
-            margin: EdgeInsets.only(top: 40.0, left: 10.0, right: 10.0),
+            margin: EdgeInsets.only(top: 50.0, left: 10.0, right: 10.0),
             decoration: BoxDecoration(
               color: Colors.white,
               border: Border.all(
