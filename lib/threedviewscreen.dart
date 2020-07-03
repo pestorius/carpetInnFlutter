65c472d3218fe0c6492345e2fc469cbb64002f62
Farhan <farhaangauhar@yahoo.com>
2020-07-04 00:34:09 +0800

    Added text description

--- lib/threedviewscreen.dart
+++ lib/threedviewscreen.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 
+class SizeConfig {
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double blockSizeHorizontal;
+  static double blockSizeVertical;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    blockSizeHorizontal = screenWidth / 100;
+    blockSizeVertical = screenHeight / 100;
+  }
+}
+
 class Threedviewscreen extends StatelessWidget {
   Threedviewscreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text('3D Viewer', style: TextStyle(color: Colors.black)),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: Container(
+              margin: EdgeInsets.only(top: 20),
+              width: SizeConfig.blockSizeHorizontal * 80,
+              child: Text(
+                'Welcome to the 3D Viewer! Use this tool to overlay the selected carpet onto a living space.',
+                style: TextStyle(fontSize: 18, height: 1.5),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
