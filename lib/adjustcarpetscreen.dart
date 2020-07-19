086f174af20fb30188e2db4b0223d04e2b0792fa
Farhan <farhaangauhar@yahoo.com>
2020-07-20 02:05:54 +0800

    Experimenting with carpet transformations

--- lib/adjustcarpetscreen.dart
+++ lib/adjustcarpetscreen.dart
@@ -1,13 +1,9 @@
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/services.dart';
-import 'dart:typed_data';
-import 'package:image/image.dart' as Img;
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:matrix_gesture_detector/matrix_gesture_detector.dart';
 
 class SizeConfig {
   static MediaQueryData _mediaQueryData;
@@ -36,16 +32,184 @@ class Adjustcarpetscreen extends StatefulWidget {
       carpetTrimmedImage: carpetTrimmedImage, backgroundImage: backgroundImage);
 }
 
+/*class AdjustcarpetscreenState extends State<Adjustcarpetscreen> {
+  AdjustcarpetscreenState(
+      {Key key, this.carpetTrimmedImage, this.backgroundImage});
+
+  final dynamic carpetTrimmedImage;
+  final dynamic backgroundImage;
+
+  Offset _offset = Offset.zero;
+  double translatex = 0,
+      translatey = 0,
+      translatez = 1,
+      endTranslatex = 0,
+      endTranslatey = 0,
+      endTranslatez = 1;
+  Offset _startingFocalPoint = Offset.zero;
+  Offset origin = Offset.zero;
+  double angle = 0, endAngle = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Transform(
+            transform: Matrix4.identity()
+              ..setEntry(3, 2, 0.001)
+              ..rotateX(0.01 * _offset.dy)
+              ..rotateY(-0.01 * _offset.dx)
+              ..translate(translatex, translatey)
+              ..scale(translatez)
+              ..rotateZ(angle),
+            alignment: FractionalOffset.center,
+            origin: origin,
+            child: GestureDetector(
+              onScaleStart: (details) => setState(() {
+                print('INTIAL: $details');
+                _startingFocalPoint = details.focalPoint;
+              }),
+              onScaleUpdate: (details) => setState(() {
+                //print('UPDATE: $details');
+                //print(_startingFocalPoint);
+                translatex = details.focalPoint.dx -
+                    _startingFocalPoint.dx +
+                    endTranslatex;
+                translatey = details.focalPoint.dy -
+                    _startingFocalPoint.dy +
+                    endTranslatey;
+                translatez = details.scale * endTranslatez;
+                angle = details.rotation + endAngle;
+              }),
+              onScaleEnd: (details) => setState(() {
+                //print('END: $details');
+                print('END: ($translatex, $translatey)');
+                endTranslatex = translatex;
+                endTranslatey = translatey;
+                endTranslatez = translatez;
+                endAngle = angle;
+              }),
+              onDoubleTap: () => setState(() {
+                translatex = 0;
+                translatey = 0;
+                translatez = 1;
+                angle = 0;
+                endTranslatex = 0;
+                endTranslatey = 0;
+                endTranslatez = 1;
+                endAngle = 0;
+              }),
+              child: Image(
+                image: carpetTrimmedImage,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}*/
+/*class AdjustcarpetscreenState extends State<Adjustcarpetscreen> {
+  AdjustcarpetscreenState(
+      {Key key, this.carpetTrimmedImage, this.backgroundImage});
+  final dynamic carpetTrimmedImage;
+  final dynamic backgroundImage;
+
+  Offset _offset = Offset.zero;
+  double translatex = 0,
+      translatey = 0,
+      translatez = 1,
+      endTranslatex = 0,
+      endTranslatey = 0,
+      endTranslatez = 1;
+  Offset _startingFocalPoint = Offset.zero;
+  Offset origin = Offset.zero;
+  double angle = 0, endAngle = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    final ValueNotifier<Matrix4> notifier = ValueNotifier(Matrix4.identity());
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+              width: SizeConfig.screenWidth,
+              height: SizeConfig.screenHeight,
+              color: Colors.black,
+              child: Image.file(backgroundImage)),
+          MatrixGestureDetector(
+              onMatrixUpdate: (m, tm, sm, rm) {
+                notifier.value = m;
+
+              },
+              child: AnimatedBuilder(
+                animation: notifier,
+                builder: (ctx, child) {
+                  return Transform(
+                    transform: notifier.value
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateX(0.01 * _offset.dy)
+                      ..rotateY(-0.01 * _offset.dx),
+                    origin: origin,
+                    child: Container(
+                      height: SizeConfig.screenHeight,
+                      child: Image(
+                        image: carpetTrimmedImage,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+
+          RotatedBox(
+            quarterTurns: 1,
+            child: Container(
+              width: SizeConfig.blockSizeHorizontal * 80,
+              child: Slider.adaptive(
+                value: _offset.dy,
+                onChanged: (newOffset) {
+                  setState(() {
+                    _offset = Offset(0, newOffset);
+                  });
+                },
+                min: -150,
+                max: 0,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}*/
+
 class AdjustcarpetscreenState extends State<Adjustcarpetscreen> {
   AdjustcarpetscreenState(
       {Key key, this.carpetTrimmedImage, this.backgroundImage});
   final dynamic carpetTrimmedImage;
   final dynamic backgroundImage;
 
+  Offset _offset = Offset.zero;
+  double translatex = 0,
+      translatey = 0,
+      translatez = 1,
+      endTranslatex = 0,
+      endTranslatey = 0,
+      endTranslatez = 1;
+  Offset _startingFocalPoint = Offset.zero;
+  Offset origin = Offset.zero;
+  double angle = 0, endAngle = 0;
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
-    print(SizeConfig.screenWidth);
     return Scaffold(
         body: Stack(children: <Widget>[
       Container(
@@ -53,12 +217,92 @@ class AdjustcarpetscreenState extends State<Adjustcarpetscreen> {
           height: SizeConfig.screenHeight,
           color: Colors.black,
           child: Image.file(backgroundImage)),
-      Align(
-          alignment: Alignment.center,
-          child: Image(
-            image: carpetTrimmedImage,
-            width: SizeConfig.blockSizeHorizontal * 80,
-          ))
+      Container(
+        width: SizeConfig.screenWidth,
+        height: SizeConfig.screenHeight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Transform(
+                  transform: Matrix4.identity()
+                    ..setEntry(3, 2, 0.001)
+                    ..rotateX(0.01 * _offset.dy)
+                    ..rotateY(-0.01 * _offset.dx)
+                    ..translate(translatex, translatey)
+                    ..scale(translatez)
+                    ..rotateZ(angle),
+                  alignment: FractionalOffset.center,
+                  origin: origin,
+                  child: GestureDetector(
+                    onScaleStart: (details) => setState(() {
+                      print('INTIAL: $details');
+                      _startingFocalPoint = details.focalPoint;
+                    }),
+                    onScaleUpdate: (details) => setState(() {
+                      //print('UPDATE: $details');
+                      //print(_startingFocalPoint);
+                      translatex = details.focalPoint.dx -
+                          _startingFocalPoint.dx +
+                          endTranslatex;
+                      translatey = details.focalPoint.dy -
+                          _startingFocalPoint.dy +
+                          endTranslatey;
+                      translatez = details.scale * endTranslatez;
+                      angle = details.rotation + endAngle;
+                    }),
+                    onScaleEnd: (details) => setState(() {
+                      //print('END: $details');
+                      print('END: ($translatex, $translatey)');
+                      endTranslatex = translatex;
+                      endTranslatey = translatey;
+                      endTranslatez = translatez;
+                      endAngle = angle;
+                      //origin = Offset(translatex, translatey);
+                    }),
+                    onDoubleTap: () => setState(() {
+                      translatex = 0;
+                      translatey = 0;
+                      translatez = 1;
+                      angle = 0;
+                      endTranslatex = 0;
+                      endTranslatey = 0;
+                      endTranslatez = 1;
+                      endAngle = 0;
+                      origin = Offset.zero;
+                    }),
+                    child: Container(
+                      //height: SizeConfig.blockSizeVertical * 90,
+                      child: Image(
+                        image: carpetTrimmedImage,
+                        width: SizeConfig.blockSizeHorizontal * 80,
+                      ),
+                    ),
+                  ),
+                ),
+                RotatedBox(
+                  quarterTurns: 1,
+                  child: Container(
+                    width: SizeConfig.blockSizeHorizontal * 80,
+                    child: Slider.adaptive(
+                      value: _offset.dy,
+                      onChanged: (newOffset) {
+                        setState(() {
+                          _offset = Offset(0, newOffset);
+                        });
+                      },
+                      min: -150,
+                      max: 0,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     ]));
   }
 }
